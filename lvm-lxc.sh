7318736d7525235aacb8f7a04ac0859ff9a7636d
Nels Nelson <nels.n.nelson@gmail.com>
2014-02-04 14:28:11 -0600

    Fixing name for template

--- lvm-lxc.sh
+++ lvm-lxc.sh
@@ -2,14 +2,14 @@
 
 set -x
 
-sudo cp lxc-rackos-minimal /usr/share/lxc/templates/lxc-rackos-minimal
+sudo cp -f lxc-rackos-minimal /usr/share/lxc/templates/lxc-rackos-minimal
 
-sudo /sbin/pvremove /dev/loop0
 sudo /sbin/vgremove lxc
+sudo /sbin/pvremove /dev/loop0
 sudo /sbin/losetup -d /dev/loop0
-/bin/rm -f /tmp/test.img
+sudo rm -f /tmp/test.img
 sudo lxc-destroy -n test
-/bin/rm -f /tmp/lxc.log
+sudo rm -f /tmp/lxc.log
 
 # Create disk iamge for lvm 
 /bin/dd if=/dev/zero of=/tmp/test.img bs=1024 count=10240
@@ -22,10 +22,13 @@ sudo /sbin/vgcreate lxc /dev/loop0
 # Create lxc instance
 sudo lxc-create -t rackos-minimal -n test -B lvm --fssize 8M -l DEBUG -o /tmp/lxc.log
 sudo lxc-start -d -n test -l DEBUG -o /tmp/lxc.log
-cat /tmp/lxc.log
+#cat /tmp/lxc.log
 
 sudo lxc-execute -n test -- cat /proc/self/mounts
-sudo lxc-execute -n test -- rm -f /tmp/garbage; /bin/dd if=/dev/zero of=/tmp/garbage bs=1024 count=10240
+sudo lxc-execute -n test -- /bin/df 
+sudo lxc-execute -n test -- rm -f /tmp/garbage
+sudo lxc-execute -n test -- /bin/dd if=/dev/zero of=/tmp/garbage bs=1025 count=10240
+sudo lxc-execute -n test -- rm -f /tmp/garbage
 
 # Cleanup
 sudo lxc-stop -n test
